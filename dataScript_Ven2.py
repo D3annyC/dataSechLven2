8db3259f0c45666046d29f51900a03e9fcb00ff1
D3anC <dean.chung@live.com>
2017-06-24 23:44:09 +0800

    bug fix

--- dataScript_Ven2.py
+++ dataScript_Ven2.py
@@ -22,7 +22,6 @@ def main():
     result = requests.get("http://218.161.81.10/app/sub4.asp?T1=VAN02")
     result.encoding='utf-8'
     root = etree.fromstring(result.content, etree.HTMLParser())
-    jsonData = "["
     data_list=[]
 
     #split string from caption
@@ -30,11 +29,11 @@ def main():
     ven =venRowData[0]
     ven =ven.split('(')
     time =ven[1].split(')')
-    sheet.update_cell(2, 1,time[0])
 
     # add ven and time
-    venInfo ='{"ven":"'+ven[0]+'","time":"'+time[0]+'",'
-    jsonData +=venInfo
+    #jsonData = "["
+    #venInfo ='{"ven":"'+ven[0]+'","time":"'+time[0]+'",'
+    #jsonData +=venInfo
     
     #formate the time and date
     data_list=[]
@@ -60,12 +59,12 @@ def main():
     rowCounter =2
     for row in root.xpath("//section/table[@class='table1']/tbody/tr[position()>1]"):
         column = row.xpath("./td/text()")
-        tmp= '"%s":"%s",' % (column[0], column[1])
+        #tmp= '"%s":"%s",' % (column[0], column[1])
         data_list.append('%s' % (column[1]))
         #sheet.update_cell(1, rowCounter,'%s' %(column[0]))
         #sheet.update_cell(2, rowCounter,'%s' %(column[1]))
         #rowCounter += 1
-        jsonData += tmp
+        #jsonData += tmp
     #sheet.resize(1)
     sheet.append_row(data_list)
     # delete last ','
